d0f96527f03f4c00292ea73794aaedaa70f6bd29
Vidrochka <Evgenuy1605@yandex.ru>
2022-01-13 16:33:44 +0300

    feat(ConfigurationSnapshot): add configuration injection

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -3,3 +3,6 @@ pub use add_hosted_service_strategy::*;
 
 mod add_background_service_strategy;
 pub use add_background_service_strategy::*;
+
+mod add_configuration_strategy;
+pub use add_configuration_strategy::*;
