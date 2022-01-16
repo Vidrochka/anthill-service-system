806b5eab7e6ca531e6a81bb913359287512c5686
Vidrochka <Evgenuy1605@yandex.ru>
2022-01-16 13:16:31 +0300

    feat(Configuration): add simple configuration

--- src/extensions/add_configuration_strategy.rs
+++ src/extensions/add_configuration_strategy.rs
@@ -13,41 +13,44 @@ use serde::Deserialize;
 
 #[async_trait]
 pub trait AddConfigurationStrategy {
-    async fn add_transient_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
-    async fn add_singleton_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
-    async fn add_scoped_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
+    async fn add_transient_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
+    async fn add_singleton_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
+    async fn add_scoped_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static;
 }
 
 #[async_trait]
 impl AddConfigurationStrategy for DependencyContext {
-    async fn add_transient_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
-        self.add_transient::<ConfigurationSnapshot<T>>(Box::new(SnapshotConfigurationBuilder::new::<T>(path))).await.unwrap();
+    async fn add_transient_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
+        self.add_transient::<T>(Box::new(ConfigurationBuilder::new::<T>(path))).await.unwrap();
     }
 
-    async fn add_singleton_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
-        self.add_singleton::<ConfigurationSnapshot<T>>(Box::new(SnapshotConfigurationBuilder::new::<T>(path))).await.unwrap();
+    async fn add_singleton_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
+        self.add_singleton::<T>(Box::new(ConfigurationBuilder::new::<T>(path))).await.unwrap();
     }
     
-    async fn add_scoped_snapshot_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
-        self.add_scoped::<ConfigurationSnapshot<T>>(Box::new(SnapshotConfigurationBuilder::new::<T>(path))).await.unwrap();
+    async fn add_scoped_configuration<T>(&self, path: String) where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
+        self.add_scoped::<T>(Box::new(ConfigurationBuilder::new::<T>(path))).await.unwrap();
     }
 }
 
-pub struct SnapshotConfigurationBuilder {
+pub struct ConfigurationBuilder {
     async_ctor: AsyncCallback<DependencyContext, BuildDependencyResult<Box<dyn Any + Send + Sync>>>,
 }
 
-impl SnapshotConfigurationBuilder {
+impl ConfigurationBuilder {
     pub fn new<T>(path: String) -> Self where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
         let ctor: AsyncCallback<DependencyContext, BuildDependencyResult<Box<dyn Any + Send + Sync>>> = Box::new(
             move |_ctx: DependencyContext| -> _{
                 let path = path.clone();
                 Box::pin(
                     async move {
-                        let configuration_snapshot = ConfigurationSnapshot::<T>::new(path).await
-                            .map_err(|err| BuildDependencyError::Custom { message: format!("{:?}", err) })?;
+                        let data = tokio::fs::read_to_string(path.clone()).await
+                            .map_err(|err| BuildDependencyError::Custom { message: format!("{:?}", ConfigurationError::ReadError { err })})?;
 
-                        Ok(Box::new(configuration_snapshot) as Box<dyn Any + Send + Sync>)
+                        let configuration: T = serde_json::from_str(&*data)
+                            .map_err(|err| BuildDependencyError::Custom { message: format!("{:?}", ConfigurationError::DeserializeError { err })})?;
+
+                        Ok(Box::new(configuration) as Box<dyn Any + Send + Sync>)
                     }
                 )
             }
@@ -57,48 +60,14 @@ impl SnapshotConfigurationBuilder {
 }
 
 #[async_trait]
-impl TypeConstructor for SnapshotConfigurationBuilder {
+impl TypeConstructor for ConfigurationBuilder {
     async fn ctor(&self, ctx: DependencyContext) -> BuildDependencyResult<Box<dyn Any + Send + Sync>> {
         (self.async_ctor)(ctx).await
     }
 }
 
-pub struct ConfigurationSnapshot<T> where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
-    path: String,
-    pub snapshot: T,
-}
-
 #[derive(Debug)]
-pub enum ConfigurationSnapshotError {
+pub enum ConfigurationError {
     ReadError { err: tokio::io::Error },
     DeserializeError { err: serde_json::Error }
-}
-
-type ConfigurationSnapshotResult<T> = Result<T, ConfigurationSnapshotError>;
-
-impl<T> ConfigurationSnapshot<T> where for<'de> T: Deserialize<'de> + Sync + Send + 'static {
-    pub async fn new(path: String) -> ConfigurationSnapshotResult<Self> {
-        let data = tokio::fs::read_to_string(path.clone()).await
-            .map_err(|err| ConfigurationSnapshotError::ReadError { err })?;
-
-        let configuration: T = serde_json::from_str(&*data)
-            .map_err(|err| ConfigurationSnapshotError::DeserializeError { err })?;
-        
-        Ok(Self {
-            path,
-            snapshot: configuration,
-        })
-    }
-
-    pub async fn update(&mut self) -> ConfigurationSnapshotResult<()> {
-        let data = tokio::fs::read_to_string(self.path.clone()).await
-            .map_err(|err| ConfigurationSnapshotError::ReadError { err })?;
-
-        let mut configuration: T = serde_json::from_str(&*data)
-            .map_err(|err| ConfigurationSnapshotError::DeserializeError { err })?;
-
-        std::mem::swap(&mut self.snapshot, &mut configuration);
-
-        Ok(())
-    }
 }
--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -4,5 +4,8 @@ pub use add_hosted_service_strategy::*;
 mod add_background_service_strategy;
 pub use add_background_service_strategy::*;
 
+mod add_snapshot_configuration_strategy;
+pub use add_snapshot_configuration_strategy::*;
+
 mod add_configuration_strategy;
 pub use add_configuration_strategy::*;
